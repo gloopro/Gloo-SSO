1bde75f60afc642d86da63a6b198519483f55822
Ofonime Francis <ofonimeusoro01@gmail.com>
2017-08-24 09:59:55 +0100

    Moved functions to AuthTrait

--- Controller/Auth.php
+++ Controller/Auth.php
@@ -54,43 +54,4 @@ abstract class Auth extends Action{
             $this->createUserAccount($resourceOwner);
         }
     }
-
-
-
-
-    public function createUserAccount(ResourceOwnerInterface $resourceOwner){
-        /**
-         * This function should handle transaction incase account linking fails
-         * You need to rollback before account creation
-         */
-        $data = $resourceOwner->toArray();
-        $websiteID = $this->storeManager->getWebsite()->getId();
-        $transactionObject = $this->getTransactionObject();
-
-        //begin transaction
-        try {
-
-        }catch(\Exception $e){
-
-        }
-        $customer = $this->customerFactory->create();
-
-        $customer->setWebsiteId($websiteID);
-        $customer->setUserid($resourceOwner->getId());
-        $customer->setName($data['name']);
-        $customer->setFirstname($data[self::FIRST_NAME]);
-        $customer->setLastname($data[self::LAST_NAME]);
-        $customer->setGender($data[self::GENDER]);
-        $customer->setEmail($data[self::EMAIL]);
-        $password = $this->generatePassword(12);
-        $customer->setPassword($password);
-
-        $customer->save();
-
-        //send confirmation email if all goes well
-        if($this->dataHelper->getConfig("send_confirmation_email")){
-            $customer->sendNewAccountEmail();
-        }
-    }
-
 }
